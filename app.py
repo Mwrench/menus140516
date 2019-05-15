5f772eb2b8bc43b7630a84a2f09411270647e5ca
Mwrench <mateus.alves.quintela@gmail.com>
2019-05-15 12:45:16 +0000

    BASES

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def index ():
+def index():
     return render_template('index.html')
 
+
 @app.route('/p1')
-def p1 ():
+def p1():
     return render_template('p1.html')
 
+
 @app.route('/p2')
-def p2 ():
+def p2():
     return render_template('p2.html')
+
+
 @app.route('/p3')
-def p3 ():
+def p3():
     return render_template('p3.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/registo')
+def registo():
+    return render_template('registo.html')
+
 
 if __name__ == '__main__':
     app.run()
