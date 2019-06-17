f647b8651b722be71683d30a00dcb26442ce0e01
Mwrench <mateus.alves.quintela@gmail.com>
2019-06-17 12:00:25 +0000

    cenas novas ver os logins

--- app.py
+++ app.py
@@ -1,6 +1,65 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import psycopg2
 
 app = Flask(__name__)
+
+
+def herokudb():
+    Host = 'ec2-54-228-207-163.eu-west-1.compute.amazonaws.com'
+    Database = 'da10tfqvt63j7j'
+    User = 'ehnytwmxiklqoj'
+    Password = '445768325993c75392e9d04a99053dc304f85c852e600a8610b1fe22ba5da756'
+    return psycopg2.connect(host=Host, database=Database, user=User, password=Password, sslmode='require')
+
+
+def gravar(v1, v2):
+    ficheiro = herokudb()
+    db = ficheiro.cursor()
+    db.execute("CREATE TABLE IF NOT EXISTS usr (usr text, pwd text)")
+    db.execute("INSERT INTO usr VALUES ( %s, %s)", (v1, v2))
+    ficheiro.commit()
+    ficheiro.close()
+    return
+
+
+def alterar(v1, v2):
+    ficheiro = herokudb()
+    db = ficheiro.cursor()
+    db.execute("UPDATE usr SET pwd = %s WHERE usr = %s", (v2, v1))
+    ficheiro.commit()
+    ficheiro.close()
+    return
+
+
+def existe(v1):
+    try:
+        import sqlite3
+        ficheiro = sqlite3.connect('db/Utilizador.db')
+        db = ficheiro.cursor()
+        db.execute("SELECT * FROM usr WHERE usr = %s", (v1,))
+        valor = db.fetchone()
+        ficheiro.close()
+    except:
+        valor = None
+    return valor
+
+
+def eliminar(v1):
+    ficheiro = herokudb()
+    db = ficheiro.cursor()
+    db.execute("DELETE FROM usr WHERE usr = %s", (v1,))
+    ficheiro.commit()
+    ficheiro.close()
+    return
+
+
+def log(v1, v2):
+    ficheiro = herokudb()
+    db = ficheiro.cursor()
+    db.execute("SELECT * FROM usr WHERE usr = %s and pwd = %s", (v1, v2,))
+    valor = db.fetchone()
+    ficheiro.close()
+    return valor
 
 
 @app.route('/')
@@ -33,14 +92,69 @@ def Jogo_do_Galo():
     return render_template('Jogo_do_Galo.html')
 
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+@app.route('/newpass', methods=['POST', 'GET'])
+def newpass():
+    erro = None
+    if request.method == "POST":
+        v1 = request.form['usr']
+        v2 = request.form['pwd']
+        v3 = request.form['cpwd']
+        if not existe(v1):
+            erro = 'O Utilizador não existe.'
+        elif v2 != v3:
+            erro = 'A palavra passe não coincide.'
+        else:
+            alterar(v1, v2)
+            erro = 'A palavra passe foi alterada.'
+    return render_template('newpass.html', erro=erro)
 
 
-@app.route('/registo')
+@app.route('/registo', methods=['POST', 'GET'])
 def registo():
-    return render_template('registo.html')
+    erro = None
+    if request.method == "POST":
+        v1 = request.form['usr']
+        v2 = request.form['pwd']
+        v3 = request.form['cpwd']
+        if existe(v1):
+            erro = 'O Utilizador já existe.'
+        elif v2 != v3:
+            erro = 'A palavra passe não coincide.'
+        else:
+            gravar(v1, v2)
+            erro = 'Utilizador registado com Sucesso.'
+    return render_template('registo.html', erro=erro)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    erro = None
+    if request.method == "POST":
+        v1 = request.form['usr']
+        v2 = request.form['pwd']
+        if not existe(v1):
+            erro = 'O Utilizador não existe.'
+        elif not log(v1, v2):
+            erro = 'A senha está incorreta.'
+        else:
+            erro = 'Bem-vindo.'
+    return render_template('login.html', erro=erro)
+
+
+@app.route('/apagar', methods=['POST', 'GET'])
+def apagar():
+    erro = None
+    if request.method == "POST":
+        v1 = request.form['usr']
+        v2 = request.form['pwd']
+        if not existe(v1):
+            erro = 'O Utilizador não existe.'
+        elif not log(v1, v2):
+            erro = 'A senha está incorreta.'
+        else:
+            eliminar(v1)
+            erro = 'Conta eliminada com Sucesso.'
+    return render_template('apagar.html', erro=erro)
 
 
 if __name__ == '__main__':
